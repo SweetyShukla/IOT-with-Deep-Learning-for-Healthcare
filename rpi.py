3bd9c792e4ad40c47a149d2962106778aec00c07
SweetyShukla <37013639+SweetyShukla@users.noreply.github.com>
2019-03-15 13:38:13 +0530

    Create rpi.py

---
+++ rpi.py
@@ -0,0 +1,17 @@
+import time
+import urllib as url
+import serial
+
+ArduinoSerial=serial.Serial("/dev/ttyACM1",115200)
+ArduinoSerial1=serial.Serial("/dev/ttyACM0",19200)
+time.sleep(2)
+
+while True:
+  s=ArduinoSerial.readline()
+  print s
+  temp=url.urlopen("https://api.thingspeak.com/update?api_key=6T00OH5K5ENKA9HX&field1="+str(s))
+  time.sleep(15)
+  t=ArduinoSerial1.readline()
+  print t
+  temp=url.urlopen("https://api.thingspeak.com/update?api_key=6T00OH5K5ENKA9HX&field2="+str(t))
+  time.sleep(15)
